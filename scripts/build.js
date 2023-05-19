29675a8bf7caa3025048ae91c66ad56314535add
choephix <choephix@gmail.com>
2023-05-19 20:12:09 +0000

    Rework index.html into a barebones image browser with sidebar for the list and a preview of the atlas image when you click on a font

--- scripts/build.js
+++ scripts/build.js
@@ -1,12 +1,12 @@
 const fs = require('fs');
 const path = require('path');
 
-function generateJSONLinksPage(folderPath, urlPrefix, pageTitle) {
-  const files = getAllJSONFiles(folderPath);
+function generatePNGLinksPage(folderPath, urlPrefix, pageTitle) {
+  const files = getAllPNGFiles(folderPath);
   const links = files.map(file => {
     const relativePath = path.relative(folderPath, file);
     const link = `${urlPrefix}/bitmap-fonts/${relativePath}`;
-    return `        <li><a href="${link}">${relativePath}</a></li>`;
+    return `        <li><a href="${link}" onclick="showPreview('${link}'); return false;">${relativePath}</a></li>`;
   });
 
   const html = `
@@ -14,12 +14,47 @@ function generateJSONLinksPage(folderPath, urlPrefix, pageTitle) {
     <html>
     <head>
       <title>${pageTitle}</title>
+      <style>
+        .container {
+          display: flex;
+          flex-direction: row;
+          height: 100vh;
+        }
+        .sidebar {
+          flex: 0 0 200px;
+          background-color: #f0f0f0;
+          padding: 10px;
+        }
+        .preview {
+          flex: 1;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+        .preview img {
+          max-width: 100%;
+          max-height: 100%;
+        }
+      </style>
+      <script>
+        function showPreview(src) {
+          const preview = document.getElementById('preview');
+          preview.innerHTML = '<img src="' + src + '">';
+        }
+      </script>
     </head>
-    <body>      
-      <h1>${pageTitle}</h1>
-      <ul>
+    <body>
+      <div class="container">
+        <div class="sidebar">
+          <h1>${pageTitle}</h1>
+          <ul>
 ${links.join('\n')}
-      </ul>
+          </ul>
+        </div>
+        <div class="preview" id="preview">
+          <p>Select a file to preview</p>
+        </div>
+      </div>
     </body>
     </html>
   `;
@@ -27,16 +62,16 @@ ${links.join('\n')}
   return html;
 }
 
-function getAllJSONFiles(folderPath) {
+function getAllPNGFiles(folderPath) {
   let files = [];
   const dirents = fs.readdirSync(folderPath, { withFileTypes: true });
 
   for (const dirent of dirents) {
     const fullPath = path.join(folderPath, dirent.name);
     if (dirent.isDirectory()) {
-      const nestedFiles = getAllJSONFiles(fullPath);
+      const nestedFiles = getAllPNGFiles(fullPath);
       files = files.concat(nestedFiles);
-    } else if (dirent.isFile() && path.extname(fullPath) === '.json') {
+    } else if (dirent.isFile() && path.extname(fullPath) === '.png') {
       files.push(fullPath);
     }
   }
@@ -44,11 +79,8 @@ function getAllJSONFiles(folderPath) {
   return files;
 }
 
-// Usage example
 const folderPath = './bitmap-fonts';
 const urlPrefix = 'https://choephix.github.io/bitmap-fonts';
 const pageTitle = 'MSDF Bitmap Fonts';
-
-const htmlPage = generateJSONLinksPage(folderPath, urlPrefix, pageTitle);
-
+const htmlPage = generatePNGLinksPage(folderPath, urlPrefix, pageTitle);
 fs.writeFileSync('index.html', htmlPage);
